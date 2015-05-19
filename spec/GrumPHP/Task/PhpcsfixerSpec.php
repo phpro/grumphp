3e5d8f7d614f5a6725c014eb0cb6f9d8b909bd7c
Igor Klipach <klipach@gmail.com>
2015-05-19 12:54:27 +0300

    Added Php-cs-fixer support

---
+++ spec/GrumPHP/Task/PhpcsfixerSpec.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace spec\GrumPHP\Task;
+
+use GrumPHP\Collection\FilesCollection;
+use GrumPHP\Configuration\GrumPHP;
+use GrumPHP\Locator\LocatorInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessBuilder;
+use SplFileInfo;
+
+class PhpcsfixerSpec extends ObjectBehavior
+{
+
+    function let(GrumPHP $grumPHP, LocatorInterface $externalCommandLocator, ProcessBuilder $processBuilder)
+    {
+        $this->beConstructedWith($grumPHP, array(), $externalCommandLocator, $processBuilder);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('GrumPHP\Task\Phpcsfixer');
+    }
+
+    function it_is_a_grumphp_external_task()
+    {
+        $this->shouldHaveType('GrumPHP\Task\ExternalTaskInterface');
+    }
+
+    function it_uses_its_external_command_locator_to_find_correct_command(LocatorInterface $externalCommandLocator)
+    {
+        $externalCommandLocator->locate('php-cs-fixer')->shouldBeCalled();
+        $this->getCommandLocation();
+    }
+
+    function it_does_not_do_anything_if_there_are_no_files(ProcessBuilder $processBuilder)
+    {
+        $processBuilder->add(Argument::any())->shouldNotBeCalled();
+        $processBuilder->setArguments(Argument::any())->shouldNotBeCalled();
+        $processBuilder->getProcess()->shouldNotBeCalled();
+
+        $files = new FilesCollection();
+        $this->run($files)->shouldBeNull();
+    }
+
+    function it_runs_the_suite(ProcessBuilder $processBuilder, Process $process)
+    {
+        $processBuilder->add('--dry-run')->shouldBeCalled();
+        $processBuilder->add('--config=default')->shouldBeCalled();
+        $processBuilder->add('--verbose')->shouldBeCalled();
+        $processBuilder->add('fix')->shouldBeCalled();
+        $processBuilder->add('file1.php')->shouldBeCalled();
+        $processBuilder->add('file2.php')->shouldBeCalled();
+        $processBuilder->setArguments(Argument::type('array'))->shouldBeCalled();
+        $processBuilder->getProcess()->willReturn($process);
+
+        $process->run()->shouldBeCalled();
+        $process->isSuccessful()->willReturn(true);
+
+        $files = new FilesCollection(array(
+            new SplFileInfo('file1.php'),
+            new SplFileInfo('file2.php'),
+        ));
+        $this->run($files);
+    }
+
+    function it_throws_exception_if_the_process_fails(ProcessBuilder $processBuilder, Process $process)
+    {
+        $processBuilder->add('--dry-run')->shouldBeCalled();
+        $processBuilder->add('--config=default')->shouldBeCalled();
+        $processBuilder->add('--verbose')->shouldBeCalled();
+        $processBuilder->add('fix')->shouldBeCalled();
+        $processBuilder->add('file1.php')->shouldBeCalled();
+        $processBuilder->setArguments(Argument::type('array'))->shouldBeCalled();
+        $processBuilder->getProcess()->willReturn($process);
+
+        $process->getOutput()->shouldBeCalled();
+        $process->run()->shouldBeCalled();
+        $process->isSuccessful()->willReturn(false);
+
+        $files = new FilesCollection(array(
+            new SplFileInfo('file1.php'),
+        ));
+        $this->shouldThrow('GrumPHP\Exception\RuntimeException')->duringRun($files);
+    }
+}
---
+++ src/GrumPHP/Task/Phpcsfixer.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace GrumPHP\Task;
+
+use GrumPHP\Collection\FilesCollection;
+use GrumPHP\Exception\RuntimeException;
+
+/**
+ * Php-cs-fixer task
+ */
+class Phpcsfixer extends AbstractExternalTask
+{
+    const COMMAND_NAME = 'php-cs-fixer';
+
+    /**
+     * @return array
+     */
+    public function getDefaultConfiguration()
+    {
+        return array(
+            'auto_fix' => false,
+            'config' => 'default',
+            'fixers' => array(),
+            'level' => '',
+            'verbose' => true,
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCommandLocation()
+    {
+        return $this->externalCommandLocator->locate(self::COMMAND_NAME);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function run(FilesCollection $files)
+    {
+        $files = $files->name('*.php');
+        if (0 === count($files)) {
+            return;
+        }
+
+        $config = $this->getConfiguration();
+
+        $this->processBuilder->setArguments(array(
+            $this->getCommandLocation(),
+            '--format=json',
+        ));
+
+        if ($config['level']) {
+            $this->processBuilder->add('--level=' . $config['level']);
+        }
+
+        if ($config['config']) {
+            $this->processBuilder->add('--config=' . $config['config']);
+        }
+
+        if (!$config['auto_fix']) {
+            $this->processBuilder->add('--dry-run');
+        }
+
+        if ($config['verbose']) {
+            $this->processBuilder->add('--verbose');
+        }
+
+        if (count($config['fixers'])) {
+            $this->processBuilder->add('--fixers=' . implode(',', $config['fixers']));
+        }
+
+        $this->processBuilder->add('fix');
+
+        $messages = array();
+        $suggest = array('You can fix all errors by running following commands:');
+        $errorCount = 0;
+        foreach ($files as $file) {
+            $processBuilder = clone $this->processBuilder;
+            $processBuilder->add($file);
+            $process = $processBuilder->getProcess();
+            $process->run();
+            if (!$process->isSuccessful()) {
+                $output = $process->getOutput();
+                $json = json_decode($output, true);
+                if ($json) {
+                    if (isset($json['files'][0]['name']) && isset($json['files'][0]['appliedFixers'])) {
+                        $messages[] = sprintf('%s) %s (%s)', ++$errorCount, $json['files'][0]['name'], implode(',', $json['files'][0]['appliedFixers']));
+                    } elseif (isset($json['files'][0]['name'])) {
+                        $messages[] = sprintf('%s) %s', ++$errorCount, $json['files'][0]['name']);
+                    }
+
+                    $suggest[] = str_replace(array("'--dry-run' ", "'--format=json' "), '', $process->getCommandLine());
+                } else {
+                    $messages[] = $output;
+                }
+
+            }
+        }
+
+        if (count($messages) && !$config['auto_fix']) {
+            throw new RuntimeException(implode("\n", $messages) . "\n" . "\n" . implode("\n", $suggest));
+        }
+    }
+}
