5cce4726fc69b5bf393c849c54a32c6f1723bef7
Toon Verwerft <toonverwerft@gmail.com>
2015-12-07 07:11:57 +0100

    Bump application version

--- src/GrumPHP/Console/Application.php
+++ src/GrumPHP/Console/Application.php
@@ -18,7 +18,7 @@ use Symfony\Component\Process\ProcessBuilder;
 class Application extends SymfonyConsole
 {
     const APP_NAME = 'GrumPHP';
-    const APP_VERSION = '0.5.2';
+    const APP_VERSION = '0.6.0';
     const APP_CONFIG_FILE = 'grumphp.yml';
 
     /**
