5c9425b7d1b73c99a34cab7afcde987a250053d4
Laurent Laville <laurent.laville@gmail.com>
2016-04-02 09:45:28 +0200

    rename task from php_blacklist to php_parser

---
+++ src/GrumPHP/Task/Phpparser.php
@@ -0,0 +1,63 @@
+<?php
+namespace GrumPHP\Task;
+
+use GrumPHP\Task\AbstractParserTask;
+use GrumPHP\Exception\RuntimeException;
+use GrumPHP\Task\Context\ContextInterface;
+use GrumPHP\Task\Context\GitPreCommitContext;
+use GrumPHP\Task\Context\RunContext;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Php Parser task
+ */
+class Parser extends AbstractParserTask
+{
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'php_parser';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigurableOptions()
+    {
+        $resolver = parent::getConfigurableOptions();
+        $resolver->setDefault('triggered_by', array('php'));
+
+        return $resolver;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function canRunInContext(ContextInterface $context)
+    {
+        return ($context instanceof GitPreCommitContext || $context instanceof RunContext);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function run(ContextInterface $context)
+    {
+        $config = $this->getConfiguration();
+
+        $files = $context->getFiles(false)->extensions($config['triggered_by']);
+        if (0 === count($files)) {
+            return;
+        }
+        $parseErrors = $this->parse($files);
+
+        if ($parseErrors->count()) {
+            throw new RuntimeException(sprintf(
+                "You have blacklisted keywords in your commit:\n%s",
+                $parseErrors->__toString()
+            ));
+        }
+    }
+}
