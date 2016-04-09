499d395dbf058a3fe5f7bdf84b5b27c51292fe94
Laurent Laville <laurent.laville@gmail.com>
2016-04-09 16:45:47 +0200

    init spec files for php_parser task

---
+++ spec/GrumPHP/Collection/ParseErrorsCollectionSpec.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace spec\GrumPHP\Collection;
+
+use GrumPHP\Parser\ParseError;
+use PhpSpec\ObjectBehavior;
+
+class ParseErrorsCollectionSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith(array(
+            new ParseError(ParseError::TYPE_ERROR, 'Found "count" function call', 'Ant.php', 58),
+        ));
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('GrumPHP\Collection\ParseErrorsCollection');
+    }
+
+    function it_is_an_array_collection()
+    {
+        $this->shouldHaveType('Doctrine\Common\Collections\ArrayCollection');
+    }
+
+    function it_should_be_parsed_as_string()
+    {
+        $this->__toString()->shouldBe('[ERROR] Ant.php: Found "count" function call on line 58');
+    }
+}
