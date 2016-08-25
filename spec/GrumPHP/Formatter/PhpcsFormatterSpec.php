e1d521ce51215ac6c63f6886cc3cfff58dae20f4
Matthias Vogel <m.vogel@andersundsehr.com>
2016-08-25 11:32:54 +0200

    [FEATURE] add suggestions to phpcs Task (use phpcbf)

---
+++ spec/GrumPHP/Formatter/PhpcsFormatterSpec.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace spec\GrumPHP\Formatter;
+
+use GrumPHP\Collection\ProcessArgumentsCollection;
+use GrumPHP\Configuration\GrumPHP;
+use GrumPHP\Formatter\PhpcsFormatter;
+use GrumPHP\Process\ProcessBuilder;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessUtils;
+
+/**
+ * @mixin PhpcsFormatter
+ */
+class PhpcsFormatterSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('GrumPHP\Formatter\PhpcsFormatter');
+    }
+
+    function it_is_a_process_formatter()
+    {
+        $this->shouldHaveType('GrumPHP\Formatter\ProcessFormatterInterface');
+    }
+
+    function it_handles_command_exceptions(Process $process)
+    {
+        $process->getOutput()->willReturn('');
+        $process->getErrorOutput()->willReturn('stderr');
+        $this->format($process)->shouldReturn('stderr');
+    }
+
+    function it_handles_invalid_json(Process $process)
+    {
+        $process->getOutput()->willReturn('invalid');
+        $this->format($process)->shouldReturn('invalid');
+    }
+
+    function it_formats_phpcs_json_output_for_single_file(Process $process)
+    {
+        $json = $this->parseJson(array(
+            '/filePath' => array('messages' => array('fixable' => true), ),
+        ));
+        $process->getOutput()->willReturn($json);
+        $this->format($process)->shouldBe('');
+    }
+
+    function it_formats_phpcs_json_output_for_multiple_files(Process $process)
+    {
+        $json = $this->parseJson(array(
+            '/filePath' => array('messages' => array('fixable' => true), ),
+            '/filePath2' => array('messages' => array('fixable' => false), ),
+        ));
+        $process->getOutput()->willReturn($json);
+        $this->format($process)->shouldBe('');
+    }
+
+    /**
+     * @param $files
+     *
+     * @return string
+     */
+    private function parseJson(array $files)
+    {
+        return PHP_EOL . json_encode(array(
+            'totals' => array(
+                'fixable' => 1
+            ),
+            'files' => $files
+        ));
+    }
+}
--- src/GrumPHP/Task/Phpcs.php
+++ src/GrumPHP/Task/Phpcs.php
@@ -2,6 +2,8 @@
 
 namespace GrumPHP\Task;
 
+use GrumPHP\Collection\ProcessArgumentsCollection;
+use GrumPHP\Formatter\PhpcsFormatter;
 use GrumPHP\Runner\TaskResult;
 use GrumPHP\Task\Context\ContextInterface;
 use GrumPHP\Task\Context\GitPreCommitContext;
@@ -13,6 +15,11 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class Phpcs extends AbstractExternalTask
 {
+    /**
+     * @var PhpcsFormatter
+     */
+    protected $formatter;
+
     /**
      * @return string
      */
@@ -70,21 +77,40 @@ class Phpcs extends AbstractExternalTask
         $config = $this->getConfiguration();
 
         $arguments = $this->processBuilder->createArgumentsForCommand('phpcs');
-        $arguments->addOptionalArgument('--standard=%s', $config['standard']);
-        $arguments->addOptionalArgument('--warning-severity=0', !$config['show_warnings']);
-        $arguments->addOptionalArgument('--tab-width=%s', $config['tab_width']);
-        $arguments->addOptionalArgument('--encoding=%s', $config['encoding']);
-        $arguments->addOptionalCommaSeparatedArgument('--sniffs=%s', $config['sniffs']);
-        $arguments->addOptionalCommaSeparatedArgument('--ignore=%s', $config['ignore_patterns']);
+        $arguments = $this->addArgumentsFromConfig($arguments, $config);
+        $arguments->add('--report-full');
+        $arguments->add('--report-json');
         $arguments->addFiles($files);
 
         $process = $this->processBuilder->buildProcess($arguments);
         $process->run();
 
         if (!$process->isSuccessful()) {
-            return TaskResult::createFailed($this, $context, $this->formatter->format($process));
+            $output = $this->formatter->format($process);
+            if (!$output) {
+                $arguments = $this->processBuilder->createArgumentsForCommand('phpcbf');
+                $arguments = $this->addArgumentsFromConfig($arguments, $config);
+                $output = $this->formatter->formatErrorMessage($arguments, $this->processBuilder);
+            }
+            return TaskResult::createFailed($this, $context, $output);
         }
 
         return TaskResult::createPassed($this, $context);
+    }
+
+    /**
+     * @param ProcessArgumentsCollection $arguments
+     * @param array $config
+     * @return ProcessArgumentsCollection
+     */
+    protected function addArgumentsFromConfig(ProcessArgumentsCollection $arguments, array $config)
+    {
+        $arguments->addOptionalArgument('--standard=%s', $config['standard']);
+        $arguments->addOptionalArgument('--warning-severity=0', !$config['show_warnings']);
+        $arguments->addOptionalArgument('--tab-width=%s', $config['tab_width']);
+        $arguments->addOptionalArgument('--encoding=%s', $config['encoding']);
+        $arguments->addOptionalCommaSeparatedArgument('--sniffs=%s', $config['sniffs']);
+        $arguments->addOptionalCommaSeparatedArgument('--ignore=%s', $config['ignore_patterns']);
+        return $arguments;
     }
 }
