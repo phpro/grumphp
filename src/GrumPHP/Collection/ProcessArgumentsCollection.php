c89c9539e9fbf769954e9fce7cfb2a0128093a44
Jeremy Huet <jeremy.huet@gmail.com>
2016-09-18 12:29:23 +0200

    Adding addOptionalSeparatedArgument and addSeparatedArgumentArray methods

--- src/GrumPHP/Collection/ProcessArgumentsCollection.php
+++ src/GrumPHP/Collection/ProcessArgumentsCollection.php
@@ -65,6 +65,18 @@ class ProcessArgumentsCollection extends ArrayCollection
 
     /**
      * @param string $argument
+     * @param string $value
+     */
+    public function addOptionalSeparatedArgument($argument, $value = null)
+    {
+        if (!is_null($value)) {
+            $this->add($argument);
+            $this->add($value);
+        }
+    }
+
+    /**
+     * @param string $argument
      * @param array  $values
      */
     public function addArgumentArray($argument, array $values)
@@ -84,6 +96,22 @@ class ProcessArgumentsCollection extends ArrayCollection
     {
         foreach ($values as $value) {
             $this->add(sprintf($argument, $value));
+            $this->add($value);
+        }
+    }
+
+    /**
+     * @param string $argument
+     * @param array  $values
+     */
+    public function addSeparatedArgumentArray($argument, array $values)
+    {
+        if (!count($values)) {
+            return;
+        }
+
+        $this->add($argument);
+        foreach ($values as $value) {
             $this->add($value);
         }
     }
