028574d006213fe2729c76f8256e29b11179592c
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-09-25 16:30:24 +0200

    PHPSpec for the run method

--- spec/GrumPHP/Task/CloverCoverageSpec.php
+++ spec/GrumPHP/Task/CloverCoverageSpec.php
@@ -3,24 +3,25 @@
 namespace spec\GrumPHP\Task;
 
 use GrumPHP\Configuration\GrumPHP;
-use GrumPHP\Formatter\ProcessFormatterInterface;
-use GrumPHP\Process\ProcessBuilder;
+use GrumPHP\Runner\TaskResult;
 use GrumPHP\Task\Context\GitCommitMsgContext;
 use GrumPHP\Task\Context\GitPreCommitContext;
 use GrumPHP\Task\Context\RunContext;
 use GrumPHP\Task\Phpunit;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * @mixin Phpunit
  */
 class CloverCoverageSpec extends ObjectBehavior
 {
-    function let(GrumPHP $grumPHP, ProcessBuilder $processBuilder, ProcessFormatterInterface $formatter)
+    function let(GrumPHP $grumPHP, Filesystem $filesystem)
     {
-        $grumPHP->getTaskConfiguration('phpunit')->willReturn(array());
-        $this->beConstructedWith($grumPHP, $processBuilder, $formatter);
+        $grumPHP->getTaskConfiguration('clover_coverage')->willReturn(array());
+        $grumPHP->getFilesystem()->willReturn($filesystem);
+        $this->beConstructedWith($grumPHP);
     }
 
     function it_is_initializable()
@@ -46,5 +47,43 @@ class CloverCoverageSpec extends ObjectBehavior
     function it_should_not_run_in_commit_message_context(GitCommitMsgContext $context)
     {
         $this->canRunInContext($context)->shouldReturn(false);
+    }
+
+    function it_runs_the_suite_but_fails_when_file_doesnt_exists(GrumPHP $grumPHP, GitCommitMsgContext $context, Filesystem $filesystem)
+    {
+        $grumPHP->getTaskConfiguration('clover_coverage')->willReturn(array(
+            'clover_file' => 'foo.bar',
+        ));
+        $filesystem->exists('foo.bar')->willReturn(false);
+        $result = $this->run($context);
+        $result->shouldBeAnInstanceOf('GrumPHP\Runner\TaskResultInterface');
+        $result->getResultCode()->shouldBe(TaskResult::FAILED);
+    }
+
+    function it_runs_the_suite(GrumPHP $grumPHP, GitCommitMsgContext $context, Filesystem $filesystem)
+    {
+        $filename = dirname(dirname(dirname(__DIR__))) . '/test/fixtures/clover_coverage/10.xml';
+        $grumPHP->getTaskConfiguration('clover_coverage')->willReturn(array(
+            'clover_file' => $filename,
+            'level' => 50,
+        ));
+        $filesystem->exists($filename)->willReturn(true);
+        $result = $this->run($context);
+        $result->shouldBeAnInstanceOf('GrumPHP\Runner\TaskResultInterface');
+        $result->getResultCode()->shouldBe(TaskResult::PASSED);
+    }
+
+    function it_runs_the_suite_but_not_reaching_coverage(GrumPHP $grumPHP, GitCommitMsgContext $context, Filesystem $filesystem)
+    {
+        $filename = dirname(dirname(dirname(__DIR__))) . '/test/fixtures/clover_coverage/10.xml';
+        $grumPHP->getTaskConfiguration('clover_coverage')->willReturn(array(
+            'clover_file' => $filename,
+            'level' => 100,
+        ));
+        $filesystem->exists($filename)->willReturn(true);
+        $result = $this->run($context);
+        $result->shouldBeAnInstanceOf('GrumPHP\Runner\TaskResultInterface');
+        $result->getResultCode()->shouldBe(TaskResult::FAILED);
+        $result->getMessage()->shouldBe('Code coverage is 60%, which is below the accepted 100%' . PHP_EOL);
     }
 }
