c7b540f38f47aab7b3b7d8ae38b95a2c2efcf309
Toon Verwerft <toonverwerft@gmail.com>
2016-10-30 19:05:23 +0100

    Add specs for the Traverser confgiurator

--- spec/GrumPHP/Parser/Php/Configurator/TraverserConfiguratorSpec.php
+++ spec/GrumPHP/Parser/Php/Configurator/TraverserConfiguratorSpec.php
@@ -4,6 +4,8 @@ namespace spec\GrumPHP\Parser\Php\Configurator;
 
 use GrumPHP\Parser\Php\Configurator\TraverserConfigurator;
 use GrumPHP\Parser\Php\Context\ParserContext;
+use GrumPHP\Parser\Php\Visitor\ConfigurableVisitorInterface;
+use GrumPHP\Parser\Php\Visitor\ContextAwareVisitorInterface;
 use PhpParser\NodeTraverserInterface;
 use PhpParser\NodeVisitor;
 use PhpSpec\ObjectBehavior;
@@ -29,13 +31,13 @@ class TraverserConfiguratorSpec extends ObjectBehavior
     }
     
     
-    function it_throw_an_exception_if_a_context_is_not_set(NodeTraverserInterface $traverser)
+    function it_throws_an_exception_if_a_context_is_not_set(NodeTraverserInterface $traverser)
     {
         $this->registerOptions(array('visitors' => array()));
         $this->shouldThrow('GrumPHP\Exception\RuntimeException')->duringConfigure($traverser);
     }
 
-    function it_throw_an_exception_if_no_visitors_are_configured(ParserContext $context, NodeTraverserInterface $traverser)
+    function it_throws_an_exception_if_no_visitors_are_configured(ParserContext $context, NodeTraverserInterface $traverser)
     {
         $this->registerContext($context);
         $this->shouldThrow('GrumPHP\Exception\RuntimeException')->duringConfigure($traverser);
@@ -59,28 +61,93 @@ class TraverserConfiguratorSpec extends ObjectBehavior
         $this->configure($traverser);
     }
     
-    function it_loads_configured_visitors_from_task_configuration()
-    {
-        
+    function it_loads_configured_visitors_from_task_configuration(
+        ContainerInterface $container,
+        ParserContext $context,
+        NodeTraverserInterface $traverser,
+        NodeVisitor $visitor
+    ) {
+        $visitorAlias = 'task_visitor';
+        $this->registerVisitorId($visitorAlias, $visitorAlias);
+        $this->registerContext($context);
+        $this->registerOptions(array('visitors' => array(
+            $visitorAlias => null
+        )));
+        $container->get($visitorAlias)->willReturn($visitor);
+
+        $traverser->addVisitor($visitor)->shouldBeCalled();
+
+        $this->configure($traverser);
     }
     
-    function it_does_not_load_unknown_visitors()
-    {
-        
+    function it_throws_an_exception_if_the_configured_visitor_could_not_be_found(
+        ParserContext $context,
+        NodeTraverserInterface $traverser
+    ) {
+        $visitorAlias = 'unknown_visitor';
+        $this->registerContext($context);
+        $this->registerOptions(array('visitors' => array(
+            $visitorAlias => null
+        )));
+
+        $this->shouldThrow('GrumPHP\Exception\RuntimeException')->duringConfigure($traverser);
     }
     
-    function it_does_not_load_unused_visitors()
-    {
-        
+    function it_does_not_load_unused_visitors(
+        ContainerInterface $container,
+        ParserContext $context,
+        NodeTraverserInterface $traverser,
+        NodeVisitor $visitor
+    ) {
+        $visitorAlias = 'unused_visitor';
+        $this->registerVisitorId($visitorAlias, $visitorAlias);
+        $this->registerContext($context);
+        $this->registerOptions(array('visitors' => array()));
+        $container->get($visitorAlias)->willReturn($visitor);
+
+        $traverser->addVisitor($visitor)->shouldNotBeCalled();
+
+        $this->configure($traverser);
     }
 
-    function it_should_append_the_context_to_a_context_aware_visitor()
-    {
+    function it_should_append_the_context_to_a_context_aware_visitor(
+        ContainerInterface $container,
+        ParserContext $context,
+        NodeTraverserInterface $traverser,
+        ContextAwareVisitorInterface $visitor
+    ) {
+        $visitorAlias = 'context_aware_visitor';
+        $this->registerVisitorId($visitorAlias, $visitorAlias);
+        $this->registerContext($context);
+        $this->registerOptions(array('visitors' => array(
+            $visitorAlias => null
+        )));
+        $container->get($visitorAlias)->willReturn($visitor);
 
+        $visitor->setContext($context)->shouldBeCalled();
+        $traverser->addVisitor($visitor)->shouldBeCalled();
+
+        $this->configure($traverser);
     }
 
-    function it_should_pass_visitor_configuration_to_a_configuration_aware_visitor()
-    {
+    function it_should_pass_visitor_configuration_to_a_configuration_aware_visitor(
+        ContainerInterface $container,
+        ParserContext $context,
+        NodeTraverserInterface $traverser,
+        ConfigurableVisitorInterface $visitor
+    ) {
+        $visitorAlias = 'configurable_visitor';
+        $configuration = array('key' => 'value');
+        $this->registerVisitorId($visitorAlias, $visitorAlias);
+        $this->registerContext($context);
+        $this->registerOptions(array('visitors' => array(
+            $visitorAlias => $configuration
+        )));
+        $container->get($visitorAlias)->willReturn($visitor);
 
+        $visitor->configure($configuration)->shouldBeCalled();
+        $traverser->addVisitor($visitor)->shouldBeCalled();
+
+        $this->configure($traverser);
     }
 }
