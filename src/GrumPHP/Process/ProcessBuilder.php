276a9816f7bdd08c363fd781e27e8a30495da7b4
Toon Verwerft <toonverwerft@gmail.com>
2016-11-08 19:20:09 +0100

    Handle windows maximum cmd string input and other platform errors as non blocking failed result.

--- src/GrumPHP/Process/ProcessBuilder.php
+++ src/GrumPHP/Process/ProcessBuilder.php
@@ -4,8 +4,10 @@ namespace GrumPHP\Process;
 
 use GrumPHP\Collection\ProcessArgumentsCollection;
 use GrumPHP\Configuration\GrumPHP;
+use GrumPHP\Exception\PlatformException;
 use GrumPHP\IO\IOInterface;
 use GrumPHP\Locator\ExternalCommand;
+use GrumPHP\Util\Platform;
 use Symfony\Component\Process\Process;
 use \Symfony\Component\Process\ProcessBuilder as SymfonyProcessBuilder;
 
@@ -61,14 +63,40 @@ class ProcessBuilder
      * @param ProcessArgumentsCollection $arguments
      *
      * @return Process
+     * @throws \GrumPHP\Exception\PlatformException
      */
     public function buildProcess(ProcessArgumentsCollection $arguments)
     {
         $builder = SymfonyProcessBuilder::create($arguments->getValues());
         $builder->setTimeout($this->config->getProcessTimeout());
         $process = $builder->getProcess();
+
         $this->logProcessInVerboseMode($process);
+        $this->guardWindowsCmdMaxInputStringLimitation($process);
+
         return $process;
+    }
+
+    /**
+     * @param Process $process
+     *
+     * @throws \GrumPHP\Exception\PlatformException
+     */
+    private function guardWindowsCmdMaxInputStringLimitation(Process $process)
+    {
+        if (!Platform::isWindows()) {
+            return;
+        }
+
+        if (strlen($process->getCommandLine()) <= Platform::WINDOWS_COMMANDLINE_STRING_LIMITATION) {
+            return;
+        }
+
+        $this->io->write('', true);
+        $this->io->write('<fg=yellow>Oh no, we hit the windows cmd input limit!</fg=yellow>', true);
+        $this->io->write('<fg=yellow>Skipping task ...</fg=yellow>');
+
+        throw PlatformException::commandLineStringLimit($process);
     }
 
     /**
