04de0b525f2b0cf5dfdba07ea7d1dbef0ac4e3ac
Daniele Scasciafratte <mte90net@gmail.com>
2017-01-10 18:39:45 +0100

    Pre push commit

---
+++ src/GrumPHP/Console/Command/Git/PrePushCommand.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace GrumPHP\Console\Command\Git;
+
+use GrumPHP\Collection\FilesCollection;
+use GrumPHP\Configuration\GrumPHP;
+use GrumPHP\Console\Helper\PathsHelper;
+use GrumPHP\Console\Helper\TaskRunnerHelper;
+use GrumPHP\IO\ConsoleIO;
+use GrumPHP\Locator\ChangedFiles;
+use GrumPHP\Runner\TaskRunnerContext;
+use GrumPHP\Task\Context\GitPreCommitContext;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * This command runs the git pre-commit hook.
+ */
+class PrePushCommand extends Command
+{
+    const COMMAND_NAME = 'git:pre-push';
+
+    /**
+     * @var GrumPHP
+     */
+    protected $grumPHP;
+
+    /**
+     * @var ChangedFiles
+     */
+    protected $changedFilesLocator;
+
+    /**
+     * @param GrumPHP $grumPHP
+     * @param ChangedFiles $changedFilesLocator
+     */
+    public function __construct(GrumPHP $grumPHP, ChangedFiles $changedFilesLocator)
+    {
+        parent::__construct();
+
+        $this->grumPHP = $grumPHP;
+        $this->changedFilesLocator = $changedFilesLocator;
+    }
+
+    /**
+     * Configure command
+     */
+    protected function configure()
+    {
+        $this->setName(self::COMMAND_NAME);
+        $this->addOption(
+            'skip-success-output',
+            null,
+            InputOption::VALUE_NONE,
+            'Skips the success output. This will be shown by another command in the git push hook chain.'
+        );
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int|void
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new ConsoleIO($input, $output);
+        $files = $this->getCommittedFiles($io);
+
+        $context = new TaskRunnerContext(
+            new GitPreCommitContext($files),
+            $this->grumPHP->getTestSuites()->getOptional('git_pre_push')
+        );
+        $context->setSkipSuccessOutput((bool) $input->getOption('skip-success-output'));
+
+        $output->writeln('<fg=yellow>GrumPHP detected a pre-push command.</fg=yellow>');
+        return $this->taskRunner()->run($output, $context);
+    }
+
+    /**
+     * @return FilesCollection
+     */
+    protected function getCommittedFiles(ConsoleIO $io)
+    {
+        if ($stdin = $io->readCommandInput(STDIN)) {
+            return $this->changedFilesLocator->locateFromRawDiffInput($stdin);
+        }
+
+        return $this->changedFilesLocator->locateFromGitRepository();
+    }
+
+    /**
+     * @return TaskRunnerHelper
+     */
+    protected function taskRunner()
+    {
+        return $this->getHelper(TaskRunnerHelper::HELPER_NAME);
+    }
+
+    /**
+     * @return PathsHelper
+     */
+    protected function paths()
+    {
+        return $this->getHelper(PathsHelper::HELPER_NAME);
+    }
+}
