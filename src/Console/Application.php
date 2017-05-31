c57aeff808b4b5a7da84a432d3b39ef347dcba02
Toon Verwerft <toonverwerft@gmail.com>
2017-05-31 19:49:48 +0200

    0.11.6 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -21,7 +21,7 @@ use Symfony\Component\Process\ProcessBuilder;
 class Application extends SymfonyConsole
 {
     const APP_NAME = 'GrumPHP';
-    const APP_VERSION = '0.11.5';
+    const APP_VERSION = '0.11.6';
 
     /**
      * @var ContainerBuilder
