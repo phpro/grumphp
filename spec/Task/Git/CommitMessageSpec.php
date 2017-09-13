3c311ef1f61571ab3edc64997a4331c3119e1bf6
Lander Vanderstraeten <lander.vanderstraeten@phpro.be>
2017-09-13 09:46:54 +0200

    Ignore max body width for commented lines in the commit message

--- spec/Task/Git/CommitMessageSpec.php
+++ spec/Task/Git/CommitMessageSpec.php
@@ -505,6 +505,21 @@ MSG;
         $result->getMessage()->shouldBe('Line 3 of commit message has > 72 characters.');
     }
 
+    function it_should_pass_when_a_line_in_the_message_is_commented_but_longer_than_72_characters(GitCommitMsgContext $context)
+    {
+        $commitMessage = <<<'MSG'
+Some summary
+
+# This line is longer than 72 characters which is clearly be seen by count.
+MSG;
+
+        $context->getCommitMessage()->willReturn($commitMessage);
+
+        $result = $this->run($context);
+        $result->shouldBeAnInstanceOf(TaskResultInterface::class);
+        $result->isPassed()->shouldBe(true);
+    }
+
     function it_should_pass_when_all_lines_in_the_message_are_fewer_than_72_characters(GitCommitMsgContext $context)
     {
         $commitMessage = <<<'MSG'
