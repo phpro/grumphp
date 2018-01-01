6a4599f905589dc5a78b52c3eff4f67da5044c10
Michaël Jacobs <michael.jacobs92@gmail.com>
2018-01-01 23:00:07 +0100

    Split YamlLinter tests by `symfony/yaml` version

--- test/Linter/Yaml/YamlLinterTest.php
+++ test/Linter/Yaml/YamlLinterTest.php
@@ -64,32 +64,91 @@ class YamlLinterTest extends TestCase
     /**
      * @test
      */
-    function it_should_be_able_to_handle_object_support()
+    function it_should_be_able_to_handle_object_support_with_yaml_legacy()
     {
-        $this->linter->setObjectSupport(true);
-
-        $fixture = 'object-support-old.yml';
-        if (YamlLinter::supportsFlags()) {
-            $fixture = YamlLinter::supportsTagsWithoutColon() ? 'object-support.yml' : 'object-support-with-colon.yml';
+        if ($this->hasSymfonyYamlVersion('3.1', '>=')) {
+            $this->markTestSkipped('This test requires Symfony YAML < 3.1');
         }
 
-        $this->validateFixture($fixture, 0);
+        $this->linter->setObjectSupport(true);
+        $this->validateFixture('object-support-old.yml', 0);
     }
 
     /**
      * @test
      */
-    function it_should_handle_exceptions_on_invalid_type()
+    function it_should_be_able_to_handle_object_support_with_yaml_lts()
     {
+        if (!(
+            $this->hasSymfonyYamlVersion('3.1', '>=') &&
+            $this->hasSymfonyYamlVersion('4.0', '<')
+        )) {
+            $this->markTestSkipped('This test requires Symfony YAML between > 3.1 and < 4.0');
+        }
+
+        $this->linter->setObjectSupport(true);
+        $this->validateFixture('object-support-with-colon.yml', 0);
+    }
+
+    /**
+     * @test
+     */
+    function it_should_be_able_to_handle_object_support_with_yaml_current()
+    {
+        if ($this->hasSymfonyYamlVersion('4.0', '<')) {
+            $this->markTestSkipped('This test requires Symfony YAML between > 4.0');
+        }
+
+        $this->linter->setObjectSupport(true);
+        $this->validateFixture('object-support.yml', 0);
+    }
+
+    /**
+     * @test
+     */
+    function it_should_handle_exceptions_on_invalid_type_with_yaml_legacy()
+    {
+        if ($this->hasSymfonyYamlVersion('3.1', '>=')) {
+            $this->markTestSkipped('This test requires Symfony YAML < 3.1');
+        }
+
         $this->linter->setObjectSupport(false);
         $this->linter->setExceptionOnInvalidType(true);
 
-        $fixture = 'object-support-old.yml';
-        if (YamlLinter::supportsFlags()) {
-            $fixture = YamlLinter::supportsTagsWithoutColon() ? 'object-support.yml' : 'object-support-with-colon.yml';
+        $this->validateFixture('object-support-old.yml', 1);
+    }
+
+    /**
+     * @test
+     */
+    function it_should_handle_exceptions_on_invalid_type_with_yaml_lts()
+    {
+        if (!(
+            $this->hasSymfonyYamlVersion('3.1', '>=') &&
+            $this->hasSymfonyYamlVersion('4.0', '<')
+        )) {
+            $this->markTestSkipped('This test requires Symfony YAML between > 3.1 and < 4.0');
         }
 
-        $this->validateFixture($fixture, 1);
+        $this->linter->setObjectSupport(false);
+        $this->linter->setExceptionOnInvalidType(true);
+
+        $this->validateFixture('object-support-with-colon.yml', 1);
+    }
+
+    /**
+     * @test
+     */
+    function it_should_handle_exceptions_on_invalid_type_with_yaml_current()
+    {
+        if ($this->hasSymfonyYamlVersion('4.0', '<')) {
+            $this->markTestSkipped('This test requires Symfony YAML between > 4.0');
+        }
+
+        $this->linter->setObjectSupport(false);
+        $this->linter->setExceptionOnInvalidType(true);
+
+        $this->validateFixture('object-support.yml', 1);
     }
 
     /**
@@ -101,5 +160,20 @@ class YamlLinterTest extends TestCase
             ['fixture' => 'valid.yml', 'errors' => 0],
             ['fixture' => 'invalid.yml', 'errors' => 1],
         ];
+    }
+
+    private function hasSymfonyYamlVersion($version, $operator = null)
+    {
+        $installedVersion = '2.8';
+
+        if (YamlLinter::supportsFlags()) {
+            $installedVersion = '3.1';
+        }
+
+        if (YamlLinter::supportsTagsWithoutColon()) {
+            $installedVersion = '4.0';
+        }
+
+        return version_compare($installedVersion, $version, $operator);
     }
 }
