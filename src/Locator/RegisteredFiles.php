cdd1152963d86dfacb1a83419a8eb121738e6590
David Zschille <david@zschille.net>
2018-01-24 15:24:40 +0100

    Ignore symlinks when registering files.

--- src/Locator/RegisteredFiles.php
+++ src/Locator/RegisteredFiles.php
@@ -31,7 +31,11 @@ class RegisteredFiles
 
         $files = [];
         foreach ($filePaths as $file) {
-            $files[] = new SplFileInfo($file, dirname($file), $file);
+            $fileInfo = new SplFileInfo($file, dirname($file), $file);
+
+            if (! $fileInfo->isLink()) {
+                $files[] = $fileInfo;
+            }
         }
 
         return new FilesCollection($files);
