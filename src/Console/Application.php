fc4eda385f04d95652f58dd9a6b4f2bc16f560b0
Toon Verwerft <toonverwerft@gmail.com>
2018-02-23 11:08:43 +0100

    0.14.0 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -21,7 +21,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 class Application extends SymfonyConsole
 {
     const APP_NAME = 'GrumPHP';
-    const APP_VERSION = '0.13.1';
+    const APP_VERSION = '0.14.0';
 
     /**
      * @var ContainerBuilder
