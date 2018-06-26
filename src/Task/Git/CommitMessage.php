0926f5e9999eb8056d61fe90636fe089cd02ae56
Scott Dutton <exussum12@users.noreply.github.com>
2018-06-26 15:30:27 +0100

    White Space

--- src/Task/Git/CommitMessage.php
+++ src/Task/Git/CommitMessage.php
@@ -174,7 +174,7 @@ class CommitMessage implements TaskInterface
 
         foreach (array_slice($lines, 2) as $index => $line) {
             if (isset($line[0]) && $line[0] == '#') {
-                continue;   
+                continue;
             }
             if (mb_strlen(rtrim($line)) > $config['max_body_width']) {
                 $errors[] = sprintf(
