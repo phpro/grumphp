876e47595ccbed0cb38afa4513356dbf3895a6ec
Lander Vanderstraeten <lander.vanderstraeten@phpro.be>
2018-06-29 15:58:57 +0200

    Fix test

--- spec/Parser/Php/PhpParserSpec.php
+++ spec/Parser/Php/PhpParserSpec.php
@@ -9,7 +9,7 @@ use GrumPHP\Parser\Php\Factory\TraverserFactory;
 use GrumPHP\Parser\Php\PhpParser;
 use GrumPHP\Util\Filesystem;
 use PhpParser\Error;
-use PhpParser\NodeTraverserInterface;
+use PhpParser\NodeTraverser;
 use PhpParser\Parser;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -21,7 +21,7 @@ class PhpParserSpec extends ObjectBehavior
         ParserFactory $parserFactory,
         TraverserFactory $traverserFactory,
         Parser $parser,
-        NodeTraverserInterface $traverser,
+        NodeTraverser $traverser,
         Filesystem $filesystem
     ) {
         $this->beConstructedWith($parserFactory, $traverserFactory, $filesystem);
@@ -34,42 +34,51 @@ class PhpParserSpec extends ObjectBehavior
     {
         $this->shouldHaveType(PhpParser::class);
     }
-// todo!
-//    function it_uses_parser_options(
-//        ParserFactory $parserFactory,
-//        TraverserFactory $traverserFactory,
-//        Parser $parser,
-//        NodeTraverserInterface $traverser
-//    ) {
-//        $file = new SplFileInfo('php://memory');
-//        $this->setParserOptions($options = ['kind' => 'php7']);
-//
-//        $parserFactory->createFromOptions($options)->shouldBeCalled()->willReturn($parser);
-//        $traverserFactory->createForTaskContext($options, Argument::that(function (ParserContext $context) use ($file) {
-//            return $context->getFile() === $file
-//                && $context->getErrors() instanceof ParseErrorsCollection;
-//        }))->shouldBeCalled()->willReturn($traverser);
-//
-//        $this->parse($file);
-//    }
 
-//    function it_parses_a_file(NodeTraverserInterface $traverser)
-//    {
-//        $file = new SplFileInfo('php://memory');
-//        $traverser->traverse([])->shouldBeCalled();
-//        $errors = $this->parse($file);
-//
-//        $errors->shouldBeAnInstanceOf(ParseErrorsCollection::class);
-//        $errors->count()->shouldBe(0);
-//    }
+    function it_uses_parser_options(
+        ParserFactory $parserFactory,
+        TraverserFactory $traverserFactory,
+        Parser $parser,
+        NodeTraverser $traverser,
+        Filesystem $filesystem
+    ) {
+        $file = new SplFileInfo('php://memory');
+        $this->setParserOptions($options = ['kind' => 'php7']);
 
-//    function it_catches_parse_exceptions(Parser $parser)
-//    {
-//        $file = new SplFileInfo('php://memory');
-//        $parser->parse(Argument::any())->willThrow(new Error('Error ....'));
-//        $errors = $this->parse($file);
-//
-//        $errors->shouldBeAnInstanceOf(ParseErrorsCollection::class);
-//        $errors->count()->shouldBe(1);
-//    }
+        $filesystem->readFromFileInfo($file)->willReturn('file content');
+
+        $parserFactory->createFromOptions($options)->shouldBeCalled()->willReturn($parser);
+        $traverserFactory->createForTaskContext($options, Argument::that(function (ParserContext $context) use ($file) {
+            return $context->getFile() === $file
+                && $context->getErrors() instanceof ParseErrorsCollection;
+        }))->shouldBeCalled()->willReturn($traverser);
+
+        $this->parse($file);
+    }
+
+    function it_parses_a_file(NodeTraverser $traverser, Filesystem $filesystem)
+    {
+        $file = new SplFileInfo('php://memory');
+
+        $filesystem->readFromFileInfo($file)->willReturn('file content');
+
+        $traverser->traverse([])->shouldBeCalled();
+        $errors = $this->parse($file);
+
+        $errors->shouldBeAnInstanceOf(ParseErrorsCollection::class);
+        $errors->count()->shouldBe(0);
+    }
+
+    function it_catches_parse_exceptions(Parser $parser, Filesystem $filesystem, SplFileInfo $file)
+    {
+        $file->getRealPath()->willReturn('a real path');
+
+        $filesystem->readFromFileInfo($file)->willReturn('file content');
+
+        $parser->parse(Argument::any())->willThrow(new Error('Error ....'));
+        $errors = $this->parse($file);
+
+        $errors->shouldBeAnInstanceOf(ParseErrorsCollection::class);
+        $errors->count()->shouldBe(1);
+    }
 }
