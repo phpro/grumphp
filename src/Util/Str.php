22f75c820cb799c0fd81e9df4f053ca1b1e1fa7c
poppabear8883 <servnx@gmail.com>
2018-09-13 15:18:52 -0400

    feat(Util): Add string helper class with contains method

---
+++ src/Util/Str.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace GrumPHP\Util;
+
+class Str
+{
+    public static function contains($haystack, $needles)
+    {
+        foreach ((array) $needles as $needle) {
+            if ($needle !== '' && mb_strpos($haystack, $needle) !== false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
