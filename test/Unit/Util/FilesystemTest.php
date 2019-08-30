db00fa9f34352887cb44fc4b5028733636005096
Pim Jansen <pimjansen@gmail.com>
2019-08-30 16:05:17 +0200

    Fixed broken test on interface spec

--- test/Unit/Util/FilesystemTest.php
+++ test/Unit/Util/FilesystemTest.php
@@ -23,7 +23,7 @@ class FilesystemTest extends FilesystemTestCase
      */
     protected $filesystem;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
         $this->config = $this->prophesize(GrumPHP::class);
