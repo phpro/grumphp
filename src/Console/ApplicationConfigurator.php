b3d80bbcbac8068f21ea5f5da18a4af01d2d471d
Toon Verwerft <toonverwerft@gmail.com>
2020-02-25 18:51:17 +0100

    0.18.0 release

--- src/Console/ApplicationConfigurator.php
+++ src/Console/ApplicationConfigurator.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputOption;
 class ApplicationConfigurator
 {
     const APP_NAME = 'GrumPHP';
-    const APP_VERSION = '0.17.2';
+    const APP_VERSION = '0.18.0';
 
     /**
      * @var GrumPHP
