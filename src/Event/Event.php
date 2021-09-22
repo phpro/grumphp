747d7f59633a1b7c8e4d4bee84ee377dd130fcac
Sergey An <ansergeyg@gmail.com>
2021-09-22 17:22:33 +0200

    Insufficient condition to switch interfaces for event interface.

--- src/Event/Event.php
+++ src/Event/Event.php
@@ -6,9 +6,11 @@ namespace GrumPHP\Event;
 
 use Symfony\Contracts\EventDispatcher\Event as SymfonyEventContract;
 use Symfony\Component\EventDispatcher\Event as SymfonyLegacyEvent;
-
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 // @codingStandardsIgnoreStart
-if (class_exists(SymfonyEventContract::class)) {
+if (class_exists(SymfonyEventContract::class) &&
+    in_array('EventDispatcherInterface', class_implements(EventDispatcher::class))) {
     class Event extends SymfonyEventContract
     {
     }
