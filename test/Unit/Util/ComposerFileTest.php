e0be168bc2fa7716d5fdbe93ba62b955c35e5f98
Jesper Skytte <jesper@skytte.it>
2023-04-11 08:45:35 +0200

    Move tests from PhpSpec to PhpUnit

--- test/Unit/Util/ComposerFileTest.php
+++ test/Unit/Util/ComposerFileTest.php
@@ -12,6 +12,19 @@ class ComposerFileTest extends TestCase
     /**
      * @test
      */
+    public function it_is_initializable(): void
+    {
+        $composerFile = new ComposerFile('composer.json', [
+            'config' => [
+                'sort-packages' => true,
+            ]
+        ]);
+        $this->assertInstanceOf(ComposerFile::class, $composerFile);
+    }
+
+    /**
+     * @test
+     */
     public function it_can_detect_the_bin_dir_using_the_bindir_configuration(): void
     {
         $composerFile = new ComposerFile('composer.json', [
@@ -43,4 +56,51 @@ class ComposerFileTest extends TestCase
         $composerFile = new ComposerFile('composer.json', []);
         $this->assertSame('vendor/bin', $composerFile->getBinDir());
     }
+
+
+    /**
+     * @test
+     */
+    public function it_knows_the_composer_file_path(): void
+    {
+        $composerFile = new ComposerFile('/my/location/composer.json', []);
+        $this->assertSame('/my/location/composer.json', $composerFile->getPath());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_have_a_default_config_path(): void
+    {
+        $composerFile = new ComposerFile(
+            '/my/location/composer.json',
+            [
+                'config' => [
+                    'sort-packages' => true,
+                ]
+            ]
+        );
+        $this->assertNull($composerFile->getConfigDefaultPath());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_have_a_custom_config_path(): void
+    {
+        $composerFile = new ComposerFile(
+            '/my/location/composer.json',
+            [
+                'config' => [
+                    'sort-packages' => true,
+                ],
+                'extra' => [
+                    'grumphp' => [
+                        'config-default-path' => 'some/folder'
+                    ]
+                ]
+            ]
+        );
+        $this->assertSame('some/folder', $composerFile->getConfigDefaultPath());
+   }
 }
