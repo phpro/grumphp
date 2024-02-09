24c16090357b015970c7e1820a0cf5fbda758a9b
Toon Verwerft <toonverwerft@gmail.com>
2024-02-09 10:24:06 +0100

    2.5.0 release

--- src/Console/ApplicationConfigurator.php
+++ src/Console/ApplicationConfigurator.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Input\InputOption;
 class ApplicationConfigurator
 {
     const APP_NAME = 'GrumPHP';
-    const APP_VERSION = '2.4.0';
+    const APP_VERSION = '2.5.0';
 
     public function configure(Application $application): void
     {
